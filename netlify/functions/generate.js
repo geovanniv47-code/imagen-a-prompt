67240fc152ce299dabf13e8199975aadc85fefd9
Geovany <geovanniv47@gmail.com>
2026-08-19 22:42:21 -0600

    Fix formatting in generate.js for model declaration

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
         "Authorization": `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        model: ""llama-3.3-70b-specdec"
+        model: "llama-3.3-70b-specdec"
 ",
         max_tokens: 1000,
         messages: [{ role: "user", content: prompt }]
